eaae7f21d5669ec80eb63e0a335ab4fc91c77ef0
andmohiko <andmohiko@gmail.com>
2022-06-01 14:22:33 +0900

    chore: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#D04DD8",
+          "secondary": "#F5A0E3",
+          "accent": "#E3CF72",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#d9f99d",
+          "warning": "#fca5a5",
+          "error": "#f87171",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
