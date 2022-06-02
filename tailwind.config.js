c96c29b420b0e00b2d15a48d656854da20e225bb
andmohiko <andmohiko@gmail.com>
2022-06-02 18:47:45 +0900

    chore: set color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   daisyui: {
     themes: [
       {
         mytheme: {
-          "primary": "#D04DD8",
-          "secondary": "#F5A0E3",
-          "accent": "#E3CF72",
-          "neutral": "#191D24",
-          "base-100": "#2A303C",
-          "info": "#3ABFF8",
-          "success": "#d9f99d",
-          "warning": "#fca5a5",
-          "error": "#f87171",
-        },
-      },
-    ],
+          primary: '#D04DD8',
+          secondary: '#F5A0E3',
+          accent: '#E3CF72',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          'base-200': '#452245',
+          info: '#3ABFF8',
+          success: '#d9f99d',
+          warning: '#fca5a5',
+          error: '#f87171'
+        }
+      }
+    ]
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')]
 }
