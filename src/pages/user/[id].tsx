588e4d9b1b73db93719d2aa879a04ac8bddb4ccc
andmohiko <andmohiko@gmail.com>
2022-09-26 15:16:04 +0900

    fix: user page layout

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -6,7 +6,7 @@ import type { NextPage } from 'next'
 
 import { HomeLayout } from '@/components/Layouts/HomeLayout'
 import { LikeAction } from '@/components/LikeAction'
-import { User } from '@/entities'
+import { me, User } from '@/entities'
 import styles from '@/styles/Home.module.css'
 
 const UserPage: NextPage = () => {
@@ -14,44 +14,34 @@ const UserPage: NextPage = () => {
   const { id: userID } = router.query
   console.log({ userID })
 
-  const user: User = {
-    userID: 'user1',
-    age: 21,
-    location: '池袋',
-    profileImageURL: '/images/splash.png',
-    selfIntroduction:
-      'コスプレをしています！\n普段は歌舞伎でサキュバスの格好をしながらシーシャを提供しています',
-    username: 'りあ'
-  }
+  const user: User = me
 
   return (
     <HomeLayout>
-      <div className={styles.main}>
-        <div className="flex flex-col gap-4 w-96 h-screen">
-          <div className="h-96 rounded-xl avatar">
-            <div className="rounded-xl">
-              <Image
-                src={user.profileImageURL}
-                alt={user.username}
-                layout="fill"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 px-2">
-            <div className="text-4xl">
-              {user.username} <span className="text-xl">({user.age})</span>
-            </div>
-            <div className="flex gap-2 items-center text-lg">
-              <GoLocation />
-              {user.location}
-            </div>
-            <div className="selfIntroduction">{user.selfIntroduction}</div>
-          </div>
+      <div className="flex overflow-y-scroll flex-col justify-start items-start pb-60 w-full">
+        <div className="relative w-full h-full border-2 border-blue-600 border-solid">
+          <Image
+            src={user.profileImageURL}
+            alt={user.username}
+            layout="fill"
+            objectFit="contain"
+            className="rounded-xl"
+          />
         </div>
-        <div className="fixed bottom-16 z-40">
-          <LikeAction />
+        <div className="flex flex-col gap-4 px-2">
+          <div className="text-4xl">
+            {user.username} <span className="text-xl">({user.age})</span>
+          </div>
+          <div className="flex gap-2 items-center text-lg">
+            <GoLocation />
+            {user.location}
+          </div>
+          <div className="selfIntroduction">{user.selfIntroduction}</div>
         </div>
       </div>
+      {/* <div className="fixed bottom-16 z-40 w-full">
+        <LikeAction />
+      </div> */}
     </HomeLayout>
   )
 }
